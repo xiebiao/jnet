b1b758f9aaa82f25f4c9f68bc7394a3c5641ed94
xiaog <joyrap@gmail.com>
2011-10-07 15:39:04 +0800

    init

---
+++ src/main/java/com/xiebiao/jnet/App.java
@@ -0,0 +1,16 @@
+package com.xiebiao.jnet;
+
+import java.nio.channels.Selector;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+	
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/xiebiao/jnet/MySocketAddress.java
@@ -0,0 +1,8 @@
+package com.xiebiao.jnet;
+
+import java.net.SocketAddress;
+
+public class MySocketAddress extends SocketAddress {
+    private static final long serialVersionUID = 9138592930412637782L;
+
+}
