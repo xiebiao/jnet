97ae8c128615d9c4c860de70d30beb1c053256f0
xiebiao <joyrap@gmail.com>
2013-04-11 18:45:10 +0800

    update

--- src/main/java/com/github/jnet/utils/IOUtils.java
+++ src/main/java/com/github/jnet/utils/IOUtils.java
@@ -1,5 +1,6 @@
 package com.github.jnet.utils;
 
+import java.io.IOException;
 import java.nio.channels.SocketChannel;
 
 import org.slf4j.Logger;
@@ -18,15 +19,17 @@ public final class IOUtils {
 	 * @return
 	 * @throws Exception
 	 */
-	public static int read(SocketChannel socket, IOBuffer buf) throws Exception {
+	public static int read(SocketChannel socket, IOBuffer buf)
+			throws IOException {
 		int len = socket.read(buf.getBuffer());
 		if (len < 0) {
-			throw new Exception("IO Error");
+			throw new IOException("IO Error");
 		}
 		return len;
 	}
+
 	public static int read(Session session, SocketChannel socket, IOBuffer buf)
-			throws Exception {
+			throws IOException {
 		logger.debug("Session[" + session.getId() + "] is reading \n"
 				+ buf.toString());
 		return read(socket, buf);
@@ -41,16 +44,16 @@ public final class IOUtils {
 	 * @throws Exception
 	 */
 	public static int write(SocketChannel socket, IOBuffer buf)
-			throws Exception {
+			throws IOException {
 		int len = socket.write(buf.getBuffer());
 		if (len < 0) {
-			throw new Exception("IO Error");
+			throw new IOException("IO Error");
 		}
 		return len;
 	}
 
 	public static int write(Session session, SocketChannel socket, IOBuffer buf)
-			throws Exception {
+			throws IOException {
 		logger.debug("Session[" + session.getId() + "] is writing \n"
 				+ buf.toString());
 		return write(socket, buf);
