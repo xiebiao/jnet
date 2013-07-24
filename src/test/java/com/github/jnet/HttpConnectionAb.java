cabf5f7a02feb609802cf0f82c6385914fcae09b
xiebiao <joyrap@gmail.com>
2013-07-24 23:46:07 +0800

    modified

--- src/test/java/com/github/jnet/HttpConnectionAb.java
+++ src/test/java/com/github/jnet/HttpConnectionAb.java
@@ -8,13 +8,13 @@ import com.github.jnet.Client;
 
 
 public class HttpConnectionAb {
-	private static String host = "localhost";
+	private static String host = "www.baidu.com";
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String header = "GET /time HTTP/1.1\r\nHost: host\r\n\r\n";
+		String header = "GET / HTTP/1.1\r\nHost: host\r\n\r\n";
 		List<InetSocketAddress> servers = new ArrayList<InetSocketAddress>();
 		servers.add(new InetSocketAddress(host, 8080));
 		Client conn = new Client(servers, 1000, 1000, 0);
